daf0a3544c4559cfe50cc92a8c96e11e258e2c55
gorgeous-lady1 <167902564+gorgeous-lady1@users.noreply.github.com>
2024-05-18 16:02:06 -0700

    Add files via upload

---
+++ guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let guest_list = ['Saad Khan', 'Ibad Khan', 'Hassan Rehman', 'Zeeshan Saleem'];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear Mr. ' + guest_list[i] + ',\n\nIt is pleasure to invite you in our party.\n\nThank You!');
+}
